16642dd1ea8f396b6cdc874be07a0ab657fae754
knogu <kotaronoguchi61@gmail.com>
2021-04-03 13:19:24 +0900

    corsの指定方法を本番見据えて変更

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module AppName
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.x.cors_allowed_origins = ENV.fetch('CORS_ALLOWED_ORIGINS', 'http://localhost:3001')
   end
 end
