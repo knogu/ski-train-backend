23082ce97a4a950fd46fe9a711d57894cf30556c
knogu <kotaronoguchi61@gmail.com>
2021-04-03 16:48:57 +0900

    CORS_ALLOWED_ORIGINSにサーバーから環境変数を渡すように変更

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module AppName
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.x.cors_allowed_origins = ENV.fetch('CORS_ALLOWED_ORIGINS', 'http://localhost:3001')
+    config.x.cors_allowed_origins = ENV['CORS_ALLOWED_ORIGINS']
   end
 end
